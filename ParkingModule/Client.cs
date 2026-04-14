68b6a8510dd5187c94e501416e2f38726d167eb2
Pyrkin <apurkin97@gmail.com>
2026-04-14 18:18:42 +0300

    make class client and parking

---
+++ ParkingModule/Client.cs
@@ -0,0 +1,61 @@
+﻿namespace ParkingModule;
+
+public class Client
+{
+    public int Id { get; set; }
+    public string NumberAuto { get; set; }
+    
+}
+
+public class RegisterClient: Client
+{
+    public string Login { get; set; }
+    public string Password { get; set; }
+}
+
+
+public class Parking
+{
+    public int Id { get; set; }
+    public string Adress { get; set; }
+    public string City { get; set; }
+    public bool IsFill { get; set; }
+    public int maxCountState { get; set; }
+    
+    public int currentCountState { get; set; }
+
+    public void AddAuto()
+    {
+        if (!IsFill)
+        {
+            currentCountState++;
+            if (currentCountState == maxCountState)
+            {
+                IsFill = true;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Нет свободных мест");
+        }
+    }
+    
+    public void DelAuto()
+    {
+        if (currentCountState != 0)
+        {
+            currentCountState--;
+            if (currentCountState < maxCountState)
+            {
+                IsFill = false;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Автомобили отсутствуют");
+        }
+        
+    }
+    
+    
+}
